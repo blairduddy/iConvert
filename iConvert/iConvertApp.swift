07843273e88c2b98ac2321402c56bf8652486cce
Blair Duddy <blairduddy@hotmail.co.uk>
2023-05-09 16:51:34 -0400

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Blair Duddy on 2023-05-09.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
